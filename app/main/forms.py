e657ddcd3f21d442e91e0444c08c5b4676acfcf9
albright.human <sungutialbright@gmail.com>
2022-05-15 13:50:13 +0300

    add forms for blogs and comments

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+
+class BlogForm(FlaskForm):
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField('Comment')
